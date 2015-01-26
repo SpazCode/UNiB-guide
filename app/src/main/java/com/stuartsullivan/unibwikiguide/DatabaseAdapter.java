2065830405a64eaae67bbbfb43a8e7fc589425bf
Stuart <spazdragon012@gmail.com>
2015-01-26 11:47:22 -0500

    Updating the Database adapter to match the new JSON format

--- app/src/main/java/com/stuartsullivan/unibwikiguide/DatabaseAdapter.java
+++ app/src/main/java/com/stuartsullivan/unibwikiguide/DatabaseAdapter.java
@@ -19,18 +19,32 @@ public class DatabaseAdapter {
     public static final String KEY_CHARACTER_IMAGE = "image";
     public static final String KEY_CHARACTER_HEALTH = "health";
     public static final String KEY_CHARACTER_TRAITS = "traits";
+    // Cancels Keys
+    public static final String KEY_CANCELS_ROWID = "_id";
+    public static final String KEY_CANCELS_NAME = "name";
+    public static final String KEY_CANCELS_VALUE = "value";
+    // Blocks Keys
+    public static final String KEY_BLOCKS_ROWID = "_id";
+    public static final String KEY_BLOCKS_NAME = "name";
+    public static final String KEY_BLOCKS_VALUE = "value";
     // Move Keys
     public static final String KEY_MOVE_ROWID = "_id";
     public static final String KEY_MOVE_CHARACTERID = "character_id";
-    public static final String KEY_MOVE_MOVETYPE = "move_type";
+    public static final String KEY_MOVE_MOVETYPEID = "move_type_id";
     public static final String KEY_MOVE_NAME = "name";
-    public static final String KEY_MOVE_DAMAGE = "damage";
-    public static final String KEY_MOVE_STARTUP = "startup";
-    public static final String KEY_MOVE_ACTIVE = "active";
-    public static final String KEY_MOVE_RECOVERY = "recovery";
-    public static final String KEY_MOVE_ADVANTAGE = "advantage";
-    public static final String KEY_MOVE_CANCELS = "cancels";
-    public static final String KEY_MOVE_BLOCKTYPE = "block_type";
+    public static final String KEY_MOVE_INPUT = "input";
+    // Move Data Keys
+    public static final String KEY_MOVEDATA_ROWID = "_id";
+    public static final String KEY_MOVEDATA_MOVEID = "move_id";
+    public static final String KEY_MOVEDATA_VERSION = "version";
+    public static final String KEY_MOVEDATA_DAMAGE = "damage";
+    public static final String KEY_MOVEDATA_STARTUP = "startup";
+    public static final String KEY_MOVEDATA_ACTIVE = "active";
+    public static final String KEY_MOVEDATA_RECOVERY = "recovery";
+    public static final String KEY_MOVEDATA_ADVANTAGE = "frame_adv";
+    public static final String KEY_MOVEDATA_CANCELS = "cancels";
+    public static final String KEY_MOVEDATA_DESCRIPTION = "description";
+    public static final String KEY_MOVEDATA_BLOCKTYPE = "block_type_id";
     // Move Type Keys
     public static final String KEY_MOVETYPE_ROWID = "_id";
     public static final String KEY_MOVETYPE_TYPENAME = "name";
@@ -43,9 +57,13 @@ public class DatabaseAdapter {
     public static final String KEY_COMBOTYPE_ROWID = "_id";
     public static final String KEY_COMBOTYPE_TYPENAME = "name";
 
+    // Database constants
     private static final String DATABASE_NAME = "UNiBGuide";
     private static final String DATABASE_CHARACTER_TABLE = "characters";
+    private static final String DATABASE_CANCELS_TABLE = "cancels";
+    private static final String DATABASE_BLOCKS_TABLE  = "blocks";
     private static final String DATABASE_MOVE_TABLE = "moves";
+    private static final String DATABASE_MOVEDATA_TABLE = "move_data";
     private static final String DATABASE_MOVETYPE_TABLE = "move_types";
     private static final String DATABASE_COMBO_TABLE = "combos";
     private static final String DATABASE_COMBOTYPE_TABLE = "combo_types";
@@ -79,18 +97,27 @@ public class DatabaseAdapter {
         return ourDB.insert(DATABASE_CHARACTER_TABLE, null, cv);
     }
 
-    public long createMoveEntry(String name, int character_id, String move_type, int damage, String startup, String active, String recovery, String advantage, String block_type) {
+    public long createMoveEntry(String name, int character_id, int move_type_id, String input) {
         ContentValues cv = new ContentValues();
         cv.put(KEY_MOVE_CHARACTERID, character_id);
         cv.put(KEY_MOVE_NAME, name);
-        cv.put(KEY_MOVE_MOVETYPE, move_type);
-        cv.put(KEY_MOVE_DAMAGE, damage);
-        cv.put(KEY_MOVE_STARTUP, startup);
-        cv.put(KEY_MOVE_ACTIVE, active);
-        cv.put(KEY_MOVE_RECOVERY, recovery);
-        cv.put(KEY_MOVE_ADVANTAGE, advantage);
-        cv.put(KEY_MOVE_BLOCKTYPE, block_type);
+        cv.put(KEY_MOVE_MOVETYPEID, move_type_id);
+        cv.put(KEY_MOVE_INPUT, input);
         return ourDB.insert(DATABASE_MOVE_TABLE, null, cv);
+    }
+
+    public long createMoveDataEntry(String version, String damage, String startup, String active, String recovery, String frameAdv, String cancels, String desc, String blocktype) {
+        ContentValues cv = new ContentValues();
+        cv.put(KEY_MOVEDATA_VERSION, version);
+        cv.put(KEY_MOVEDATA_DAMAGE, damage);
+        cv.put(KEY_MOVEDATA_STARTUP, startup);
+        cv.put(KEY_MOVEDATA_ACTIVE, active);
+        cv.put(KEY_MOVEDATA_RECOVERY, recovery);
+        cv.put(KEY_MOVEDATA_ADVANTAGE, frameAdv);
+        cv.put(KEY_MOVEDATA_CANCELS, cancels);
+        cv.put(KEY_MOVEDATA_DESCRIPTION, desc);
+        cv.put(KEY_MOVEDATA_BLOCKTYPE, blocktype);
+        return ourDB.insert(DATABASE_MOVEDATA_TABLE, null, cv);
     }
 
     public long createComboEntry(int character_id, String type, String sequence) {
@@ -179,16 +206,19 @@ public class DatabaseAdapter {
         String result = "";
 
         // The query
-        String query = "SELECT " + KEY_MOVE_NAME + ", " +
-                KEY_MOVE_MOVETYPE + ", " +
-                KEY_MOVE_DAMAGE + ", " +
-                KEY_MOVE_STARTUP + ", " +
-                KEY_MOVE_ACTIVE + ", " +
-                KEY_MOVE_RECOVERY + ", " +
-                KEY_MOVE_ADVANTAGE + ", " +
-                KEY_MOVE_BLOCKTYPE + " " +
-                "FROM " + DATABASE_MOVE_TABLE + " " +
-                "WHERE " + KEY_MOVE_CHARACTERID + " = " + id;
+        String query = "SELECT m." + KEY_MOVE_NAME + ", " +
+                "mt." + KEY_MOVETYPE_TYPENAME + ", " +
+                "md." + KEY_MOVEDATA_DAMAGE + ", " +
+                "md." + KEY_MOVEDATA_STARTUP + ", " +
+                "md." + KEY_MOVEDATA_ACTIVE + ", " +
+                "md." + KEY_MOVEDATA_RECOVERY + ", " +
+                "md." + KEY_MOVEDATA_ADVANTAGE + ", " +
+                "md." + KEY_MOVEDATA_BLOCKTYPE + ", " +
+                "md." + KEY_MOVEDATA_CANCELS + " " +
+                "FROM " + DATABASE_MOVE_TABLE + " m, " + DATABASE_MOVETYPE_TABLE + " mt, " + DATABASE_MOVEDATA_TABLE + " md " +
+                "WHERE m." + KEY_MOVE_CHARACTERID + " = " + id +
+                "AND md." + KEY_MOVEDATA_ROWID + " = m." + KEY_MOVE_ROWID +
+                "AND m." + KEY_MOVE_MOVETYPEID + " = mt." + KEY_MOVETYPE_ROWID;
         // Set up the cursor
         Cursor cur = ourDB.rawQuery(query, null);
 
@@ -199,12 +229,13 @@ public class DatabaseAdapter {
                 // build the result string
                 result += cur.getString(0) + ":" +
                           cur.getString(1) + ":" +
-                          cur.getInt(2) + ":" +
+                          cur.getString(2) + ":" +
                           cur.getString(3) + ":" +
                           cur.getString(4) + ":" +
                           cur.getString(5) + ":" +
                           cur.getString(6) + ":" +
-                          cur.getString(7) + "\n";
+                          cur.getString(7) + ":" +
+                          cur.getString(8) + "\n";
 
                 // move to next line
                 cur.moveToNext();
@@ -271,19 +302,34 @@ public class DatabaseAdapter {
                 KEY_CHARACTER_IMAGE + " TEXT," +
                 KEY_CHARACTER_HEALTH + " INTEGER, " +
                 KEY_CHARACTER_TRAITS + " TEXT)";
+        private static final String CREATE_CANCELS_TABLE = "CREATE TABLE " + DATABASE_CANCELS_TABLE + " (" +
+                KEY_CANCELS_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_CANCELS_NAME + " TEXT NOT NULL UNIQUE, " +
+                KEY_CANCELS_VALUE + " TEXT)";
+        private static final String CREATE_BLOCKS_TABLE = "CREATE TABLE " + DATABASE_BLOCKS_TABLE + " (" +
+                KEY_BLOCKS_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_BLOCKS_NAME + " TEXT NOT NULL UNIQUE, " +
+                KEY_BLOCKS_VALUE + " TEXT)";
         private static final String CREATE_MOVE_TABLE = "CREATE TABLE " + DATABASE_MOVE_TABLE + " (" +
                 KEY_MOVE_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 KEY_MOVE_CHARACTERID + " INTEGER NOT NULL, " +
                 KEY_MOVE_NAME + " INTEGER NOT NULL, " +
-                KEY_MOVE_MOVETYPE + " TEXT NOT NULL, "+
-                KEY_MOVE_DAMAGE + " INTEGER, " +
-                KEY_MOVE_STARTUP + " TEXT, " +
-                KEY_MOVE_ACTIVE + " TEXT,  " +
-                KEY_MOVE_RECOVERY + " TEXT, " +
-                KEY_MOVE_ADVANTAGE + " TEXT, " +
-                KEY_MOVE_BLOCKTYPE + " TEXT, " +
+                KEY_MOVE_MOVETYPEID + " INTEGER NOT NULL, "+
+                KEY_MOVE_INPUT + " TEXT, " +
                 "FOREIGN KEY (" + KEY_MOVE_CHARACTERID + ") REFERENCES " + DATABASE_CHARACTER_TABLE + "(" + KEY_CHARACTER_ROWID + ")" +
-                "FOREIGN KEY (" + KEY_MOVE_MOVETYPE + ") REFERENCES " + DATABASE_MOVETYPE_TABLE + "(" + KEY_MOVETYPE_ROWID + "))";
+                "FOREIGN KEY (" + KEY_MOVE_MOVETYPEID + ") REFERENCES " + DATABASE_MOVETYPE_TABLE + "(" + KEY_MOVETYPE_ROWID + "))";
+        private static final String CREATE_MOVEDATA_TABLE = "CREATE TABLE " + DATABASE_MOVEDATA_TABLE + "(" +
+                KEY_MOVEDATA_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_MOVEDATA_MOVEID + " INTEGER NOT NULL, " +
+                KEY_MOVEDATA_VERSION + " TEXT NOT NULL, " +
+                KEY_MOVEDATA_DAMAGE + " TEXT, " +
+                KEY_MOVEDATA_STARTUP + " TEXT, " +
+                KEY_MOVEDATA_ACTIVE + " TEXT, " +
+                KEY_MOVEDATA_RECOVERY + " TEXT, " +
+                KEY_MOVEDATA_ADVANTAGE + " TEXT, " +
+                KEY_MOVEDATA_BLOCKTYPE + " TEXT, " +
+                KEY_MOVEDATA_DESCRIPTION + " TEXT, " +
+                "FOREIGN KEY (" + KEY_MOVEDATA_MOVEID + ") REFERENCES " + DATABASE_MOVE_TABLE + "( " + KEY_MOVE_ROWID + "))";
         private static final String CREATE_MOVETYPE_TABLE = "CREATE TABLE " + DATABASE_MOVETYPE_TABLE + " (" +
                 KEY_MOVETYPE_ROWID + " INTEGER PRIMARY KEY, " +
                 KEY_MOVETYPE_TYPENAME + " TEXT NOT NULL)";
@@ -304,23 +350,32 @@ public class DatabaseAdapter {
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            Log.i(TAG, "Created: " + CREATE_CHARACTER_TABLE);
+            Log.i(TAG, "Created: " + DATABASE_CHARACTER_TABLE);
             db.execSQL(CREATE_CHARACTER_TABLE);
+            Log.i(TAG, "Created: " + DATABASE_CANCELS_TABLE);
+            db.execSQL(CREATE_CANCELS_TABLE);
+            Log.i(TAG, "Created: " + DATABASE_BLOCKS_TABLE);
+            db.execSQL(CREATE_BLOCKS_TABLE);
             Log.i(TAG, "Created: " + DATABASE_MOVETYPE_TABLE);
             db.execSQL(CREATE_MOVETYPE_TABLE);
             Log.i(TAG, "Created: " + DATABASE_COMBOTYPE_TABLE);
             db.execSQL(CREATE_COMBOTYPE_TABLE);
-            Log.i(TAG, "Created: " + CREATE_MOVE_TABLE);
+            Log.i(TAG, "Created: " + DATABASE_MOVE_TABLE);
             db.execSQL(CREATE_MOVE_TABLE);
-            Log.i(TAG, "Created: " + CREATE_COMBO_TABLE);
+            Log.i(TAG, "Created: " + DATABASE_MOVEDATA_TABLE);
+            db.execSQL(CREATE_MOVEDATA_TABLE);
+            Log.i(TAG, "Created: " +DATABASE_COMBO_TABLE);
             db.execSQL(CREATE_COMBO_TABLE);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + CREATE_COMBOTYPE_TABLE);
-            db.execSQL("DROP TABLE IF EXISTS " + CREATE_MOVETYPE_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_BLOCKS_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_CANCELS_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_COMBOTYPE_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_MOVETYPE_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + DATABASE_MOVE_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_MOVEDATA_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + DATABASE_COMBO_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + DATABASE_CHARACTER_TABLE);
             onCreate(db);
